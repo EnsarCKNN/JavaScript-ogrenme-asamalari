483d9f5ced0585560531560316a9fbb60c26560c
EnsarCKN <105236753+EnsarCKNN@users.noreply.github.com>
2022-05-26 22:11:43 +0300

    Zaman
    Mart 2022

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*
+function myjs() {
+    document.getElementById('demo').innerHTML='bu işte tammamdır';
+}
+function myjs() {
+    document.getElementById('demo').innerHTML = 'Function Özelliği Çalışıyor';
+} // bu function özelliğide böyle kullanılmakta
+*/
